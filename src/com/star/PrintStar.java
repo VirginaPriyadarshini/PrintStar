f0295397cc178bf79522df4d60c94db7e409731f
VirginaPriyadarshini <38617112+VirginaPriyadarshini@users.noreply.github.com>
2018-04-23 01:44:30 +0530

    Initial commit

---
+++ src/com/star/PrintStar.java
@@ -0,0 +1,22 @@
+package com.star;
+
+/**
+ * @author vpriyada
+ *
+ */
+public class PrintStar {
+
+  public static void main(String args[]) {
+
+    for (int i = 0; i <= 3; i++) {
+      for (int j = 0; j < i; j++) {
+        if (i == 1) {
+          System.out.print(" ");
+        }
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+
+}
